033b60d3409504e6598467b187884a66d796ee88
SaqqaraWright <wrightsaqqara@gmail.com>
2022-03-29 13:59:21 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Button is hidden from the screen
+function hideButton(element){
+element.remove();
+}
+//Text on button changes to Logout when clicked
+function changeText(element) {
+    element.innerText= "Logout";
+}
